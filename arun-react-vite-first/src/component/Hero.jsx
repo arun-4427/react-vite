ecdcf0f2eeefd8d451293516ba42857b76d1bb7d
arun bikram rai <arunchamling999gmail.com>
2024-10-21 02:08:48 +0545

    first react

---
+++ arun-react-vite-first/src/component/Hero.jsx
@@ -0,0 +1,23 @@
+const HeroSection = () => {
+    return <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>
+                YOUR FEET DESRVER THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP WITH OUT SHOES.
+            </p>
+        <div  className="hero-btn">
+            <button>SHOP NOW</button>
+            <button className="secondary-btn">Category</button>
+        
+        </div>
+        </div>
+      
+        <div className="hero-image">
+            <img src="/images/shoe.png" alt="img" />
+        </div>
+        
+
+    </main>
+};
+
+export default HeroSection;
